fc74698505f8427920177b915568781db4322790
mpourmoghaddamzand <mortezapourmoghaddam99@gmail.com>
2025-06-22 13:02:36 +0330

    Flip Grid

--- src/pages/Animation.jsx
+++ src/pages/Animation.jsx
@@ -1,4 +1,3 @@
-import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { Flip } from 'gsap/all'
 import React, { useEffect, useRef, useState } from 'react'
@@ -6,45 +5,40 @@ import React, { useEffect, useRef, useState } from 'react'
 gsap.registerPlugin(Flip)
 
 const Animation = () => {
-    const [colmn, setColmn] = useState('grid-cols-2')
+    const [colmn, setColmn] = useState('grid-cols-3')
     const ref = useRef()
     const flipState = useRef(null)
 
-    // این تابع برای کلیک
     const handleClick = () => {
-        const items = ref.current.querySelectorAll('p')
+        // گرفتن تمام عناصر داخل container شامل دکمه
+        const items = ref.current.querySelectorAll('p, button')
         flipState.current = Flip.getState(items)
-        setColmn(prev => prev === 'grid-cols-3' ? 'grid-cols-2' : 'grid-cols-3')
+        setColmn(prev => (prev === 'grid-cols-3 scale-105' ? 'grid-cols-6 scale-100' : 'grid-cols-3 scale-105'))
     }
 
-    // انیمیشن روی تغییر colmn
     useEffect(() => {
         if (flipState.current) {
             Flip.from(flipState.current, {
-                duration: 1.2,
+                delay: .2,
+                duration: 1,
                 ease: 'power2.inOut',
-                stagger: 0.03,
+                stagger: 0.02,
             })
             flipState.current = null
         }
     }, [colmn])
-
-    // انیمیشن اولیه (وقتی صفحه لود شد)
     useEffect(() => {
-        const items = ref.current.querySelectorAll('p')
+        const items = ref.current.querySelectorAll('p , button')
         flipState.current = Flip.getState(items)
         // تغییر اولیه colmn بعد از mount تا انیمیشن اجرا شه
-        setColmn('grid-cols-3')
+        setColmn('grid-cols-6 scale-100')
     }, [])
 
     return (
-        <div className="p-10">
-            <div
-                ref={ref}
-                className={`grid ${colmn} gap-4 transition-all`}
-            >
+        <div className="p-10 h-screen" ref={ref}>
+            <div className={`grid ${colmn} gap-4`}>
                 {Array.from({ length: 12 }).map((_, i) => (
-                    <p key={i} className="bg-blue-200 p-4 text-center">
+                    <p key={i} className="bg-blue-200 p-4 border-2 text-center">
                         {i + 1}
                     </p>
                 ))}
